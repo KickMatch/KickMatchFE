31dbce92c860b1620650760bd9ab9e7a68154f38
William Phelps <williamphelps13@gmail.com>
2021-10-17 15:41:16 -0600

    Add imports for components

--- src/components/Athlete/Athlete.js
+++ src/components/Athlete/Athlete.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Athlete.css';
+import AthleteInterestedTeams from '../AthleteInterestedTeams/AthleteInterestedTeams';
+import AthleteProfile from '../AthleteProfile/AthleteProfile';
+import AthleteSearch from '../AthleteSearch/AthleteSearch';
+import Header from '../Header/Header';
 
 const Athlete = () => {
 
