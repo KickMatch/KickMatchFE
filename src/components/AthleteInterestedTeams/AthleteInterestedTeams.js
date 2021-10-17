62eb457b041148039345f367eccecfdbfd91af80
William Phelps <williamphelps13@gmail.com>
2021-10-17 15:28:50 -0600

    Add AthleteInterestedTeams files

---
+++ src/components/AthleteInterestedTeams/AthleteInterestedTeams.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AthleteInterestedTeams.css';
+
+const AthleteInterestedTeams = () => {
+
+  
+  return (
+    
+  );
+}
+
+export default AthleteInterestedTeams;
