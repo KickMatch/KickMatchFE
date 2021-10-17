8e05438359eacfd252455c4f0b3c9259d9c36fc9
William Phelps <williamphelps13@gmail.com>
2021-10-17 15:41:16 -0600

    Add imports for components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
+import Athlete from '../Athlete/Athlete';
+import Error from '../Error/Error';
+import Loading from '../Loading/Loading';
+import Login from '../Login/Login';
+import RegistrationAthlete from '../RegistrationAthlete/RegistrationAthlete';
+import RegistrationTeam from '../RegistrationTeam/RegistrationTeam';
+import Team from '../Team/Team';
+
+
 
 const App = () => {
 
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Team.css';
+import TeamInterestedAthletes from '../TeamInterestedAthletes/TeamInterestedAthletes';
+import TeamProfile from '../TeamProfile/TeamProfile';
+import TeamSearch from '../TeamSearch/TeamSearch';
+import Header from '../Header/Header';
 
 const Team = () => {
 
