3197fbe344a925adaefa9470bbf269b5b9657710
William Phelps <williamphelps13@gmail.com>
2021-10-17 15:30:17 -0600

    Add Registration files

---
+++ src/components/RegistrationAthlete/RegistrationAthlete.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RegistrationAthlete.css';
+
+const RegistrationAthlete = () => {
+
+  
+  return (
+    
+  );
+}
+
+export default RegistrationAthlete;
---
+++ src/components/RegistrationTeam/RegistrationTeam.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RegistrationTeam.css';
+
+const RegistrationTeam = () => {
+
+  
+  return (
+    
+  );
+}
+
+export default RegistrationTeam;
