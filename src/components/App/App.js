ae5dc78e328be5b1b54f896e2a010f76a55984c6
William Phelps <williamphelps13@gmail.com>
2021-10-17 14:39:43 -0600

    Add main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
+import React from 'react'
 import './App.css';
+import { Route } from 'react-router-dom'
+
 
 const App = () => {
+
+  
   return (
-    <div className="App">
-   <h1>FUTBOLLLLLLL!</h1>
-    </div>
+    <main>
+      <h1>Kick Match</h1>
+    </main>
   );
 }
 
