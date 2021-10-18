2576db2a615f00b2381a8f8d4246c1a243d3ec6a
StevenTheBearded <stevenberg22@gmail.com>
2021-10-17 21:05:19 -0400

    Add links to click and route page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
 
   
   return (
-    
+    <section>
+      <Link to='/athlete'> <h1> Athlete Log In </h1> </Link>
+      <Link to='/team'> <h1> Team Log In </h1> </Link>
+    </section>
   );
 }
 
