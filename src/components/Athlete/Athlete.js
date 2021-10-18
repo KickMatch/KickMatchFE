50667795f82cf1326ac1324e856455d721dd170e
StevenTheBearded <stevenberg22@gmail.com>
2021-10-18 17:20:16 -0400

    Comment out Athlete

--- src/components/Athlete/Athlete.js
+++ src/components/Athlete/Athlete.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import './Athlete.css';
-import AthleteInterestedTeams from '../AthleteInterestedTeams/AthleteInterestedTeams';
-import AthleteProfile from '../AthleteProfile/AthleteProfile';
-import AthleteSearch from '../AthleteSearch/AthleteSearch';
-import Header from '../Header/Header';
+// import React from 'react';
+// import { Link, Switch, Route } from 'react-router-dom';
+// import './Athlete.css';
+// import AthleteInterestedTeams from '../AthleteInterestedTeams/AthleteInterestedTeams';
+// import AthleteProfile from '../AthleteProfile/AthleteProfile';
+// import AthleteSearch from '../AthleteSearch/AthleteSearch';
+// import Header from '../Header/Header';
+// import { useParams } from 'react-router';
 
-const Athlete = () => {
+// const Athlete = () => {
+//   const {id} = useParams()
+//   // const test = 'athlete'
+//   // console.log('props.props in athlete: ', props.props);
+//   return (
+//     <section>
+//       <Header />
+//       {/* <Switch>
+//        <Route path={`/athlete/${id}`} render={() => {<AthleteProfile />}} />
+//        <Route path={`/athlete/${id}/AthleteSearch`} render={() => {<AthleteSearch />}} />
+//        <Route path={`/athlete/${id}`} render={() => {<AthleteProfile />}} />
+//       </Switch> */}
+//     </section>
+//   );
+// }
 
-  // const test = 'athlete'
-  // console.log('props.props in athlete: ', props.props);
-  return (
-    <section>
-      <Header />
-      <AthleteProfile />
-    </section>
-  );
-}
-
-export default Athlete;
+// export default Athlete;
