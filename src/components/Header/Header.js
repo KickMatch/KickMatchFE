3aacebd89714f98bdc58569069dc3de771f44a22
StevenTheBearded <stevenberg22@gmail.com>
2021-10-18 11:52:04 -0400

    Add useParams hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import './Header.css';
 
-const Header = ({site}) => {
+const Header = () => {
 
-  console.log('site: ', site);
+  const {id} = useParams()
+
   return (
     <header>
-      <Link to='/athlete'> <h1>Profile</h1> </Link>
+      <Link to={`/athlete/${id}`}> <h1>Profile</h1> </Link>
       <Link to='/athlete/search'> <h1>Search</h1> </Link>
-      <Link to='/athlete/interested-teams'> <h1>Interested Teams</h1> </Link>
+      <Link to='/athlete/interested-teams'> <h1>Interested Parties</h1> </Link>
       <Link to='/'> <h1>Log Out</h1></Link>
     </header>
   );
