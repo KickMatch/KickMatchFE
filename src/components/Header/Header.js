76910c5814b00ee6b0f1cd31a886653f9806d13d
StevenTheBearded <stevenberg22@gmail.com>
2021-10-17 21:05:03 -0400

    Add links for routing page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
 
   
   return (
-    
+    <header>
+      <Link to='/athlete'> <h1>Profile</h1> </Link>
+      <Link to='/athlete/search'> <h1>Search</h1> </Link>
+      <Link to='/athlete/interested-teams'> <h1>Interested Teams</h1> </Link>
+      <Link to='/'> <h1>Log Out</h1></Link>
+    </header>
   );
 }
 
