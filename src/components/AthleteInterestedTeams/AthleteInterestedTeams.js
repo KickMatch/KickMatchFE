2593089b9708961ac98d355937a9ddc9f01e5b93
StevenTheBearded <stevenberg22@gmail.com>
2021-10-17 21:04:09 -0400

    Add components to render

--- src/components/AthleteInterestedTeams/AthleteInterestedTeams.js
+++ src/components/AthleteInterestedTeams/AthleteInterestedTeams.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './AthleteInterestedTeams.css';
+import Header from '../Header/Header';
 
 const AthleteInterestedTeams = () => {
 
   
   return (
-    
+    <section>
+      <Header />
+      <h1>Atthlete interested teams</h1>
+    </section>
   );
 }
 
