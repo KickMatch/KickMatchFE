4e50f38a6aeaeb8f9267c6cae0720fb1ea7ff468
StevenTheBearded <stevenberg22@gmail.com>
2021-10-19 16:31:28 -0400

    Create queries file to store query structures

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -1,9 +1,9 @@
-import {gql} from '@apollo/client'
+import { gql } from '@apollo/client'
 
 export const LOAD_ALL_TALENT = gql `
     query {
       allTalent {
-        id,
+        id
         name
       }
     }
