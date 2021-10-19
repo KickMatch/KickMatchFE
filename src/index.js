017f63f9ed6708d239cc5f472f9ddfaf7122e1ad
StevenTheBearded <stevenberg22@gmail.com>
2021-10-18 21:05:48 -0400

    Add graphQL imports

--- src/index.js
+++ src/index.js
@@ -3,10 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink, from, networkErrors } from '@apollo/client';
+import {onError } from '@apollo/client/link/error'
 
-const router =
+const errorLink = onError(( {graphqlErrors, networkErrors }) => {
+  if(graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`GraphQL Error: ${message}`)
+    })
+  }
+})
+
+// const link = from([
+//   new HttpLink({uri: "https://frozen-waters-94259.herokuapp.com/graphql"})
+// ])
+
+const client = new ApolloClient({
+  uri: "https://frozen-waters-94259.herokuapp.com/graphql",
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>;
-
-ReactDOM.render(router, document.getElementById('root'));
+    <ApolloClient client={client}>
+      <React.StrictMode>
+          <App />
+        </React.StrictMode>
+    </ApolloClient>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
