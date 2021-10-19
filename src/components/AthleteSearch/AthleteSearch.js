6a0dc8943f527dbe953bfef324f2446618e1ae4a
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-18 19:48:03 -0600

    Incorporate Header section

--- src/components/AthleteSearch/AthleteSearch.js
+++ src/components/AthleteSearch/AthleteSearch.js
@@ -7,26 +7,29 @@ import { useParams } from 'react-router';
 const AthleteSearch = () => {
   const {id} = useParams()
   return (
-    <section class='AthleteSearch'>
-      <form class='AthleteForm'>
-        <h3>Search by:</h3>
-        <input
-          type='text'
-          placeholder='Sport Club Name'
-          name='sportClub'
-          // value={sportClubName}
-        />
-        <select>
-          <option>20 Miles</option>
-          <option>50 Miles</option>
-          <option>100 Miles</option>
-          <option>150 or more</option>
-        </select>
-        <button className='SearchBtn'>Search Teams</button>
-      </form>
-      <div className='SearchedTeamsContainer'>
-        <h3>This is where the Team Component and carousel should be</h3>
-      </div>
+    <section>
+      <Header />
+      <section class='AthleteSearch'>
+        <form class='AthleteForm'>
+          <h3>Search by:</h3>
+          <input
+            type='text'
+            placeholder='Sport Club Name'
+            name='sportClub'
+            // value={sportClubName}
+          />
+          <select>
+            <option>20 Miles</option>
+            <option>50 Miles</option>
+            <option>100 Miles</option>
+            <option>150 or more</option>
+          </select>
+          <button className='SearchBtn'>Search Teams</button>
+        </form>
+        <div className='SearchedTeamsContainer'>
+          <h3>This is where the Team Component and carousel should be</h3>
+        </div>
+      </section>
     </section>
   );
 }
