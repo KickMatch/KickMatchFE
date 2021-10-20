b6edd975f1853b720ef96b5cc988a4591ec541e9
StevenTheBearded <stevenberg22@gmail.com>
2021-10-19 21:00:10 -0400

    Modify mock data

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,31 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
 
-const Login = () => {
-
-  const athletes = [
-    {cagetory: 'athlete', name: 'Will', age: 24, position: 'Forward', id: 1},
-    {cagetory: 'athlete', name: 'Andrey', age: 26, position: 'Goalee', id: 2},
-    {cagetory: 'athlete', name: 'Steven', age: 32, position: 'Striker', id: 3}
-  ]
+const Login = ({ getUser }) => {
 
   const teams = [
     {name: 'Denver FC', id: 1},
     {name: 'Boulder FC', id: 2},
     {name: 'Colorado Springs FC', id: 3}
   ]
-
+  
+  const athletes = [
+    {cagetory: 'athlete', name: 'Will', age: 24, position: 'Forward', id: 1, height: '5\'1', primaryPosition: 1, secondaryPosition: 2, dominantFoot: 'left', zipCode: 80014},
+    {cagetory: 'athlete', name: 'Andrey', age: 26, position: 'Goalee', id: 2, height: '6\'2', primaryPosition: 5, secondaryPosition: 1, dominantFoot: 'right', zipCode: 80019},
+    {cagetory: 'athlete', name: 'Steven', age: 32, position: 'Striker', id: 3, height: '5\'7', primaryPosition: 4, secondaryPosition: 7, dominantFoot: 'left', zipCode: 80022}
+  ]
   
   return (
     <section>
       <ul>
       {
-        athletes.map((athlete) => {
-          return (<li key={athlete.id}>
-            <Link to={`/athlete/${athlete.id}`}>
-              {athlete.name}
-            </Link>
-          </li>)
+        athletes.map(athlete => {
+          return (
+            <li onClick={(e) =>getUser(athlete)} key={athlete.id}>
+              <Link to={`/athlete/${athlete.id}`}>
+                {athlete.name}
+              </Link>
+            </li>
+          )
         })
       }
       {
