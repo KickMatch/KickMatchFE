793de0a458a762fa2f594a2d7b2348784009c556
StevenTheBearded <stevenberg22@gmail.com>
2021-10-19 22:51:08 -0400

    Modify mockdata

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,9 +11,9 @@ const Login = ({ getUser }) => {
   ]
   
   const athletes = [
-    {cagetory: 'athlete', name: 'Will', age: 24, position: 'Forward', id: 1, height: '5\'1', primaryPosition: 1, secondaryPosition: 2, dominantFoot: 'left', zipCode: 80014, weight: 150, goalsMadeLast: 3, fortyYard: 4.5, vertJump: 35, personJugRec: 100, talents: ['string1', 'string2', 'string3'], awards: ['award1', 'award2', 'award3']},
-    {cagetory: 'athlete', name: 'Andrey', age: 26, position: 'Goalee', id: 2, height: '6\'2', primaryPosition: 5, secondaryPosition: 1, dominantFoot: 'right', zipCode: 80019, weight: 200, goalsMadeLast: 3, fortyYard: 4.5, vertJump: 35, personJugRec: 100, talents: ['string1', 'string2', 'string3'], awards: ['award1', 'award2', 'award3']},
-    {cagetory: 'athlete', name: 'Steven', age: 32, position: 'Striker', id: 3, height: '5\'7', primaryPosition: 4, secondaryPosition: 7, dominantFoot: 'left', zipCode: 80022, weight: 130, goalsMadeLast: 3, fortyYard: 4.5, vertJump: 35, personJugRec: 100, talents: ['string1', 'string2', 'string3'], awards: ['award1', 'award2', 'award3']}
+    {cagetory: 'athlete', firstName: 'Will', lastName: 'Smith', age: 24, position: 'Forward', id: 1, height: '5\'1', primaryPosition: 1, secondaryPosition: 2, dominantFoot: 'left', zipCode: 80014, weight: 150, goalsMadeLast: 3, fortyYard: 4.5, vertJump: 35, personJugRec: 100, talents: ['string1', 'string2', 'string3'], awards: ['award1', 'award2', 'award3']},
+    {cagetory: 'athlete', firstName: 'Andrey', lastName: 'Johnson', age: 26, position: 'Goalee', id: 2, height: '6\'2', primaryPosition: 5, secondaryPosition: 1, dominantFoot: 'right', zipCode: 80019, weight: 200, goalsMadeLast: 3, fortyYard: 4.5, vertJump: 35, personJugRec: 100, talents: ['string1', 'string2', 'string3'], awards: ['award1', 'award2', 'award3']},
+    {cagetory: 'athlete', firstName: 'Steven', lastName: 'Escobar', age: 32, position: 'Striker', id: 3, height: '5\'7', primaryPosition: 4, secondaryPosition: 7, dominantFoot: 'left', zipCode: 80022, weight: 130, goalsMadeLast: 3, fortyYard: 4.5, vertJump: 35, personJugRec: 100, talents: ['string1', 'string2', 'string3'], awards: ['award1', 'award2', 'award3']}
   ]
   
   return (
@@ -24,7 +24,7 @@ const Login = ({ getUser }) => {
           return (
             <li onClick={(e) =>getUser(athlete)} key={athlete.id}>
               <Link to={`/athlete/${athlete.id}`}>
-                {athlete.name}
+                {`${athlete.firstName} ${athlete.lastName}`}
               </Link>
             </li>
           )
