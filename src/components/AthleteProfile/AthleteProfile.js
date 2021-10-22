771010ba72ce99739b3e369d3a7a43327cae79da
StevenTheBearded <stevenberg22@gmail.com>
2021-10-21 21:33:03 -0400

    Add conditional rendering to display mobile

--- src/components/AthleteProfile/AthleteProfile.js
+++ src/components/AthleteProfile/AthleteProfile.js
@@ -3,23 +3,32 @@ import Header from '../Header/Header';
 import CarouselComp from '../Carousel/Carousel';
 import AthleteStatContainer from '../AthleteStatContainer/AthleteStatContainer';
 import Bio from '../Bio/Bio';
+import MobileAthleteStat from './MobileAthleteStat/MobileAthleteStat'
+import { useState } from 'react';
 
 const AthleteProfile = ({ data }) => {
-  
+  const [mobile, setMobile] = useState('3')
 
+  
   return (
     <>
       <Header />
-      <main>
-        <AthleteStatContainer athlete={data} />
-        <div className='divider'></div>
-        <CarouselComp data={data}/>
-        <div className='divider'></div>
-        <Bio data={data} />
-      </main>
-
+        {
+          !mobile ? 
+            <main>
+              <AthleteStatContainer athlete={data} />
+              <div className='divider'></div>
+              <CarouselComp data={data}/>
+              <div className='divider'></div>
+              <Bio data={data} />
+            </main>
+          : 
+            <MobileAthleteStat />
+        }
     </>
   );
 }
 
 export default AthleteProfile;
+
+// return (isMember ? '$2.00' : '$10.00');
