29c03141015639a08c2a6f5b74e9e71491faf667
StevenTheBearded <stevenberg22@gmail.com>
2021-10-21 21:33:13 -0400

    Create files

---
+++ src/components/AthleteProfile/MobileAthleteStat/MobileAthleteStat.js
@@ -0,0 +1,11 @@
+import './MobileAthleteStat.css';
+
+const MobileAthleteStat = () => {
+  return (
+    <main className='player-stat-container'>
+      <h1> MOBILE ATHLETE STAT</h1>
+    </main>
+  )
+}
+
+export default MobileAthleteStat;
