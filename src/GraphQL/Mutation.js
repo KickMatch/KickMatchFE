837f140c54be94f13310243f5711eddbfc494103
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-23 15:21:00 -0600

    Return data from mutation file

--- src/GraphQL/Mutation.js
+++ src/GraphQL/Mutation.js
@@ -3,7 +3,7 @@ import {gql} from '@apollo/client'
 export const CREATE_MATCH = gql `
   mutation createMatch($talent_id: Number! $sport_club_id: Number!) {
     createMatch(talent_id: $talent_id sport_club_id: $sport_club_id:) {
-
+      talent_id
     }
   }
 `
