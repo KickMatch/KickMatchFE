1e53e5471829e75bb711dea2960d9ba96d775b7e
StevenTheBearded <stevenberg22@gmail.com>
2021-10-23 15:31:09 -0400

    Added properties to be rendered

--- src/components/AthleteProfile/MobileAthleteStat/MobileAthleteStat.js
+++ src/components/AthleteProfile/MobileAthleteStat/MobileAthleteStat.js
@@ -1,36 +1,35 @@
 import './MobileAthleteStat.css';
 
 const MobileAthleteStat = ({ athlete }) => {
-  // import { useEffect, useState } from 'react';
-  // import { useWindowWidth } from '@react-hook/window-size'
-  // const [windowWidth, setWindowWidth] = useState(0)
-  // const [mobile, setMobile] = useState(false)
-
-  // const size = useWindowWidth()
-  
-  // const handleWindow = () => {
-  //   setWindowWidth(size)
-  // }
-
-  // useEffect(() => {
-  //   handleWindow()
-  // }, [size])
 
   const { avatar, playerNum, firstName, lastName, age, height, primaryPosition, secondaryPosition, dominantFoot, zipCode, weight, goalsMadeLast, fortyYard, vertJump, personJugRec, talents, awards} = athlete
   
+  // console.log('athlete: ', athlete);
   return (
-    <main className='mobile-player-stat-container'>
-      <span className='mobile-name-container'>
-        <h1>{playerNum}</h1>
-        <h3>{firstName} {lastName}</h3>
-      </span>
-      <span className='mobile-avatar-container'>
-        <img alt='avatar' className='mobile-avatar' src={avatar} />
-      </span>
-      <span className=''>
-        <h1>{height} | {weight} | {age}</h1>
-      </span>
-    </main>
+      <main className='mobile-player-stat-container'>
+        <span className='mobile-name-container'>
+          <h1>{playerNum}</h1>
+          <h3>{firstName} {lastName}</h3>
+        </span>
+        <span className='mobile-avatar-container'>
+          <img alt='avatar' className='mobile-avatar' src={avatar} />
+        </span>
+        <span className=''>
+          <h1>{height} | {weight} | {age}</h1>
+        </span>
+        <span className='mobile-attribute-container' >
+          <p>Zipcode: {zipCode}</p>
+          <p>Primary Position: {primaryPosition}</p>
+          <p>Secondary Position: {secondaryPosition}</p>
+          <p>Dominat Foot: {dominantFoot}</p>
+        </span>
+        <span className='mobile-stat-container' >
+          <p>Goals Last Season: {goalsMadeLast}</p>
+          <p>Vertical Jump: {vertJump}</p>
+          <p>40 Yard Dash: {fortyYard}</p>
+          <p>Personal Juggling Record: {personJugRec}</p>
+        </span>
+      </main>
   )
 }
 
