e375fe6db04ea794dafffb5bc50356caff023828
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-23 14:54:41 -0600

    Test query for allTaltens

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -3,8 +3,14 @@ import { gql } from '@apollo/client'
 export const LOAD_ALL_TALENT = gql `
     query {
       allTalent {
-        id
-        name
+        id,
+        name,
+        age,
+        height,
+        weight,
+        primaryPosition,
+        secondaryPosition,
+        videoUrl
       }
     }
   `
