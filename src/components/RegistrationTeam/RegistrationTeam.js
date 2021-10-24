16a3faa3465cd0eabbdef07a349a06fad4a0de10
William Phelps <williamphelps13@gmail.com>
2021-10-24 15:21:04 -0600

    Add state

--- src/components/RegistrationTeam/RegistrationTeam.js
+++ src/components/RegistrationTeam/RegistrationTeam.js
@@ -6,3 +6,22 @@ const positionList = ['Goalie', 'Fullback', 'Sweeper/Stopper', 'Center Midfielde
 
 const states = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
 
+const RegistrationTeam = () => {
+  const [registrationTeam, setRegistrationTeam] = useState({
+    name: '',
+    location: '',
+    email: '',
+    phone: '',
+    league: '',
+    tournaments: '',
+    coachResume: '',
+    positionsOpen: [],
+  });
+
+  const [address, setAddress] = useState({
+    street: '',
+    city: '',
+    state: '',
+    zipCode: '',
+  }); 
+
