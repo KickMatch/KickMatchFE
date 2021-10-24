04c0acc46abe8e94cf72ecc54906f2af6a252eb9
William Phelps <williamphelps13@gmail.com>
2021-10-24 15:20:49 -0600

    Add selection data arrays

--- src/components/RegistrationTeam/RegistrationTeam.js
+++ src/components/RegistrationTeam/RegistrationTeam.js
@@ -1,12 +1,8 @@
-import React from 'react';
+import { StoreWriter } from '@apollo/client/cache/inmemory/writeToStore';
+import React, { useState, useEffect } from 'react';
 import './RegistrationTeam.css';
 
-const RegistrationTeam = () => {
+const positionList = ['Goalie', 'Fullback', 'Sweeper/Stopper', 'Center Midfielder', 'Outside Midfielder', 'Striker'];
 
-  
-  return (
-    <h1>Registration team</h1>
-  );
-}
+const states = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
 
-export default RegistrationTeam;
