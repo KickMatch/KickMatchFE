2af7d7c0520434db9acc1f21bd2c8193d36264c5
William Phelps <williamphelps13@gmail.com>
2021-10-23 19:41:02 -0600

    Add athlete stats inputs

--- src/components/RegistrationAthlete/RegistrationAthlete.js
+++ src/components/RegistrationAthlete/RegistrationAthlete.js
@@ -49,7 +49,28 @@ const RegistrationAthlete = () => {
         <label>Email - 
           <input type='email' name='email' value={registration.email} onChange={e => handleChange(e)}></input>
         </label>
-       
+        <label>Height (in) - 
+          <input type='number' name='height' value={registration.hweighteight} onChange={e => handleChange(e)}></input>
+        </label>
+        <label>Weight (lbs) - 
+          <input type='number' name='weight' value={registration.weight} onChange={e => handleChange(e)}></input>
+        </label>
+        <label>Age - 
+          <select name='age' value={registration.age} onChange={e => handleChange(e)}>
+            <option value='8'>8</option>
+            <option value='9'>9</option>
+            <option value='10'>10</option>
+            <option value='11'>11</option>
+            <option value='12'>12</option>
+            <option value='13'>13</option>
+            <option value='14'>14</option>
+            <option value='15'>15</option>
+            <option value='16'>16</option>
+            <option value='17'>17</option>
+            <option value='18'>18</option>
+          </select>
+        </label>
+      
     </section>
   );
 }
