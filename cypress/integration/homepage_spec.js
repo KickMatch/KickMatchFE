a753a9c0c869ce62c19ce5aff357d18a62707f61
William Phelps <williamphelps13@gmail.com>
2021-10-25 14:55:50 -0600

    Rename spec

--- cypress/integration/homepage_spec.js
+++
@@ -1,9 +0,0 @@
-describe('Main page rendering', () => {
-  describe('Should be able to see main page', () => {
-    it('User should see header', () => {
-      cy
-        .visit('https://kick-match-fe.herokuapp.com/')
-        expect(true).to.be.true
-    })
-  })
-})
