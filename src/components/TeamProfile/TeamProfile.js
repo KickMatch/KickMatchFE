c55ca0e17cb8f18f39ed68f4a646ba46a0fa6067
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-24 19:23:52 -0600

    Refactor keys to render on Teamprofile component

--- src/components/TeamProfile/TeamProfile.js
+++ src/components/TeamProfile/TeamProfile.js
@@ -12,12 +12,12 @@ const TeamProfile = ({teamInfo, getId}) => {
       <div className='teamContactInfo'>
         <img className='team-prof-logo' src='https://upload.wikimedia.org/wikipedia/commons/4/4f/United_States_women%27s_national_soccer_team_logo.svg'></img>
         <p className='team-prof-title'><b>Team Name:</b></p>
-        <p className='team-prof-info'>{teamInfo.teamName}</p>
+        <p className='team-prof-info'>{teamInfo.name}</p>
         <p className='team-prof-title'><b>Location:</b></p>
-        <p className='team-prof-info'>{teamInfo.teamLocation}</p>
+        <p className='team-prof-info'>{teamInfo.location}</p>
         <p className='team-prof-title'><b>Contact Info:</b></p>
-        <p className='team-prof-info'><i>Email:</i> fc.denver@gmail.com</p>
-        <p className='team-prof-info'><i>Phone:</i> 722-223-2365</p>
+        <p className='team-prof-info'><i>Email:</i> {teamInfo.email}</p>
+        <p className='team-prof-info'><i>Phone:</i> {teamInfo.phone}</p>
       </div>
       <div className='teamSportingInfo'>
         <p className='team-prof-title'><b>League:</b></p>
@@ -25,11 +25,9 @@ const TeamProfile = ({teamInfo, getId}) => {
         <p className='team-prof-title'><b>Tournaments:</b></p>
           <p className='team-prof-info'><i>{teamInfo.tournaments}</i></p>
         <p className='team-prof-title'><b>Couch Resume:</b></p>
-          <p className='team-prof-info'>{teamInfo.couchResume}</p>
+          <p className='team-prof-info'>{teamInfo.coachResume}</p>
         <p className='team-prof-title'><b>Currently Looking:</b></p>
-          <p className='team-prof-info'><i>{teamInfo.openPositions[0]}</i></p>
-          <p className='team-prof-info'><i>{teamInfo.openPositions[1]}</i></p>
-          <p className='team-prof-info'><i>{teamInfo.openPositions[2]}</i></p>
+          <p className='team-prof-info'><i>{teamInfo.currentlyLooking}</i></p>
       <button className='match-btn' onClick={getIdTeam}>Interested in Team</button>
       </div>
     </section>
