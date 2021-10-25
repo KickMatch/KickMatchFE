65ce7c95b1dd7ea449c961dd326aebccf9d620fa
William Phelps <williamphelps13@gmail.com>
2021-10-25 14:20:56 -0600

    Fix team checkboxes

--- src/components/RegistrationTeam/RegistrationTeam.js
+++ src/components/RegistrationTeam/RegistrationTeam.js
@@ -25,11 +25,18 @@ const RegistrationTeam = () => {
     zipCode: '',
   }); 
 
-  useEffect(() => {
-    setRegistrationTeam(otherState => ({ ...otherState, location: `${address.street} ${address.city}, ${address.state} ${address.zipCode}`}))
-  }, [address])
+  const handleChange = (e) => {
+    setRegistrationTeam(otherState => ({ ...otherState, [e.target.name]: e.target.value}));
+  }
 
-  
+  const handleAddress = (e) => {
+    setAddress(otherState => ({ ...otherState, [e.target.name]: e.target.value}));
+  }
+
+  useEffect(() => {
+    setRegistrationTeam(otherState => ({ ...otherState, location: `${address.street} ${address.city}, ${address.state} ${address.zipCode}`}));
+  }, [address]);
+
   const [checkedPosition, setCheckedPosition] = useState(new Array(positionList.length).fill(false))
 
   const handlePosition = (spot) => {
@@ -40,55 +47,64 @@ const RegistrationTeam = () => {
     setCheckedPosition(updatedCheckedPosition)
   }
 
-  const handleAddress = (e) => {
-    setAddress(otherState => ({ ...otherState, [e.target.name]: e.target.value}))
-  }
+  useEffect(() => {
+    let indexTrue = [];
 
-  const handleChange = (e) => {
-    setRegistrationTeam(otherState => ({ ...otherState, [e.target.name]: e.target.value}))
-  }
+    for (let i = 0; i < checkedPosition.length; i++) {
+      checkedPosition[i] && indexTrue.push(i);
+    };
+
+    const resultingPositions = indexTrue.map(checked => positionList[checked]).join(', ');
+
+    setRegistrationTeam(otherState => ({ ...otherState, positionsOpen: resultingPositions}));
+
+  }, [checkedPosition]);
 
   return (
     <section className='registration-team-page'>
-      <header>
+      <header className='registration-team-header'>
         <h1>Create a New Team Profile</h1>
       </header>
       <section className='team-form'>
-        <label>Team Name -
-          <input type='text' name='name' value={registrationTeam.name} onChange={e => handleChange(e)}></input>
-        </label>
-        <label>Street - 
-          <input type='text' name='street' value={address.street} onChange={e => handleAddress(e)}></input>
-        </label>
-        <label>City - 
-          <input type='text' name='city' value={address.city} onChange={e => handleAddress(e)}></input>
-        </label>
-        <label>State - 
-          <select name='state' value={address.state} onChange={e => handleAddress(e)}>
-            <option value='' disabled>- Select One-</option>
-            {states.map((state, index) => <option key={index} value={state}>{state}</option>)}
-          </select>
-        </label>
-        <label>Zip Code -
-          <input type='text' name='zipCode' value={address.zipCode} onChange={e => handleAddress(e)}></input>
-        </label>
-        <label>Email - 
-          <input type='email' name='email' value={registrationTeam.email} onChange={e => handleChange(e)}></input>
-        </label>
-        <label>Phone Number - 
-          <input type='tel' name='phone' value={registrationTeam.phone} onChange={e => handleChange(e)}></input>
-        </label>
-        <label>League - 
-          <input type='text' name='league' value={registrationTeam.league} onChange={e => handleChange(e)}></input>
-        </label>
-        <label>Tournaments - 
-          <input type='text' name='tournaments' value={registrationTeam.tournaments} onChange={e => handleChange(e)}></input>
-        </label>
-        <label>Coach Resume - 
-          <input type='text' name='coachResume' value={registrationTeam.coachResume} onChange={e => handleChange(e)}></input>
-        </label>
+        <section className='athlete-stats'>
+          <label>Team Name -
+            <input type='text' name='name' value={registrationTeam.name} onChange={e => handleChange(e)}></input>
+          </label>
+          <label>Street - 
+            <input type='text' name='street' value={address.street} onChange={e => handleAddress(e)}></input>
+          </label>
+          <label>City - 
+            <input type='text' name='city' value={address.city} onChange={e => handleAddress(e)}></input>
+          </label>
+          <label>State - 
+            <select name='state' value={address.state} onChange={e => handleAddress(e)}>
+              <option value='' disabled>- Select One-</option>
+              {states.map((state, index) => <option key={index} value={state}>{state}</option>)}
+            </select>
+          </label>
+          <label>Zip Code -
+            <input type='text' name='zipCode' value={address.zipCode} onChange={e => handleAddress(e)}></input>
+          </label>
+          <label>Email - 
+            <input type='text' name='email' value={registrationTeam.email} onChange={e => handleChange(e)}></input>
+          </label>
+          <label>Phone Number - 
+            <input type='text' name='phone' value={registrationTeam.phone} onChange={e => handleChange(e)}></input>
+          </label>
+          <label>League - 
+            <input type='text' name='league' value={registrationTeam.league} onChange={e => handleChange(e)}></input>
+          </label>
+          <label>Tournaments - 
+            <input type='text' name='tournaments' value={registrationTeam.tournaments} onChange={e => handleChange(e)}></input>
+          </label>
+          <label>Coach Resume - 
+            <input type='text' name='coachResume' value={registrationTeam.coachResume} onChange={e => handleChange(e)}></input>
+          </label>
+        </section>
         <label>Positions Open -
-            {positionList.map((position, index) => <label><input type='checkbox' name={position} value={position} checked={checkedPosition[index]} onChange={() => handlePosition(index)}></input>{position}</label>)}
+          <div className='positions-container'>
+            {positionList.map((position, index) => <label className='checkbox-label'><input type='checkbox' name={position} value={position} checked={checkedPosition[index]} onChange={() => handlePosition(index)}></input>{position}<span></span></label>)}
+          </div>
         </label>
       </section>
     </section>
