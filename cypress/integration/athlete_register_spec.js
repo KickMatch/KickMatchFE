4bb5135decdfa62c54ae5567af6fa82f03136249
William Phelps <williamphelps13@gmail.com>
2021-10-25 17:43:41 -0600

    Add athlete setup

---
+++ cypress/integration/athlete_register_spec.js
@@ -0,0 +1,13 @@
+describe('Athlete Register Page', () => {
+  
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    })
+
+  it('Should have app url when displaying login page', () => {
+    cy.url()
+    .should('eq', 'http://localhost:3000/')
+  });
+
+  
+})
