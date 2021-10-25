feccd70d041c07d9ba3f51d52216d01df862be09
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-25 16:52:27 -0600

    Set up mutations for match between team and athlete

--- src/GraphQL/Mutations.js
+++ src/GraphQL/Mutations.js
@@ -1,9 +1,52 @@
 import {gql} from '@apollo/client'
 
 export const CREATE_MATCH = gql `
-  mutation createMatch($sportClubId: String! $talentId: String!) {
-    createMatch(sportClubId: $sportClubId talentId: $talentId) {
-      talentId
+  mutation createMatch($sportClubId: Int! $talentId: Int!) {
+    createMatch(input: {sportClubId: $sportClubId talentId: $talentId}) {
+      match {
+        sportClubId
+        talentId
+      }
     }
   }
 `
+
+export const CREATE_TALENT = gql `
+  mutation createTalent(
+    $name: String! 
+    $age: Int!
+    $height: String!
+    $weight: Int!
+    $primaryPosition: String!
+    $secondaryPosition: String!
+    $videoUrl: String!
+    $zipcode: Int!
+    $email: String!
+    $dominantFoot: String!
+    $goalsMadeLs: Int!
+    $verticalJump: Int!
+    $forthyDash: !Int
+    $jugglingRecord: Int!
+    $talents: String!
+    ) {
+    createTalent( input: {
+      name: $name
+      age: $age
+      height: $height
+      weigth: $weight
+      primaryPosition: $primaryPosition
+      secondaryPosition: $secondaryPosition
+      videoUrl: $videoUrl
+      zipcode: $zipcode
+      email: $email
+      dominantFoot: $dominantFoot
+      goalsMadeLs: $goalsMadeLs
+      verticalJump: $verticalJump
+      forthyDash: $forthyDash
+      jugglingRecord: $jugglingRecord
+      talents: $talents
+      )
+    }
+  }
+`
+
