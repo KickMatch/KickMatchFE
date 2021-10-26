8d74ff295a51eed3e2e0976e8e45d630b9bb24b9
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-26 11:47:21 -0600

    Add disabled functionality for button

--- src/components/TeamProfile/TeamProfile.js
+++ src/components/TeamProfile/TeamProfile.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 const TeamProfile = ({teamInfo, getId}) => {  
 
   const [matchBtn, setMatchBtn] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const getIdTeam = () => {
     setMatchBtn(true);
