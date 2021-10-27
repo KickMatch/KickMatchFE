99fef0b883625779c18121983085d087146163a6
StevenTheBearded <stevenberg22@gmail.com>
2021-10-26 20:12:37 -0400

    Creat initial component

---
+++ src/components/AthleteMatches/AthleteMatches.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Header from '../Header/Header';
+import MobileHeader from '../MobileHeader/MobileHeader';
+import { useEffect, useState } from 'react';
+import { useWindowWidth } from '@react-hook/window-size'
+import { ATHLETE_MATCHES } from '../../GraphQL/Queries';
+import { useQuery } from '@apollo/client';
+import { useParams } from 'react-router';
+import './AthleteMatches.css';
+import Loading from '../Loading/Loading';
+
+
+const AthleteMatches = () => {
+  const [windowWidth, setWindowWidth] = useState(0)
+  const [mobile, setMobile] = useState(false)
+  const [teams, setTeams] = useState({})
+  const [pageLoading, setPageLoading] = useState(true)
+  const { id } = useParams()
+  const { data } = useQuery(ATHLETE_MATCHES(id))
+  const size = useWindowWidth()
+  
+  const handlePageLoad = () => {
+    if(data){
+      setPageLoading(false);
+      setTeams(data.matchTalent);
+    }
+  }
+
+  const handleWindow = () => {
+    setWindowWidth(size)
+    handlePageLoad()
+    windowWidth >= 767 ? setMobile(false) : setMobile(true)
+  }
+
+  const handleMap = arr => arr.map((item, index) => {
+    return(
+        <ul key={index}>
+          <li className='match-list'>Email: {item.email}</li>
+          <li className='match-list'>Name: {item.name}</li>
+          <li className='match-list'>ZipCode: {item.zipcode}</li>
+        </ul>
+    )
+  })
+
+  useEffect(() => {
+    handleWindow()
+    handlePageLoad()
+   }, [windowWidth, size, data])
+
+  const AthleteMatchesContainer = () => {
+
+    return ( // Container rendered underneath header
+      <>
+      <main className='match-main'>
+        <div>{handleMap(teams)}</div>
+      </main>
+      </>
+    )
+  }
+
+   return ( //Rendered to page
+    <>
+      {!mobile ? <Header /> : <MobileHeader />}
+      {pageLoading ? <Loading /> : <AthleteMatchesContainer/> }
+    </>
+  );
+}
+
+export default AthleteMatches;
