42d1100196c6504ee7d3e7393dafb7f53e3b5a6b
Andrey Bermudez <andrey.bermudez92@gmail.com>
2021-10-27 09:04:12 -0600

    Add test for elements displayed on the search window

--- cypress/integration/athlete_search_page.js
+++ cypress/integration/athlete_search_page.js
@@ -9,4 +9,15 @@ describe('Athlete Search Page', () => {
     cy.url()
       .should('eq', 'https://kick-match-fe.herokuapp.com/athlete/1/search')
   });
+
+  it('Athlete Search page should contain a header and a form section to find teams and create matches', () => {
+    cy.get('.header').should('exist')
+    cy.get('.athlete-search').should('exist')
+  });
+
+  it('Athlete Search page should contain a header with the logo and a nav bar, showing that the search button is selected', () => {
+    cy.get('.header').get('.header-logo')
+    cy.get('.header').get('.header-active > .header-text').contains('Search')
+  });
+
 });
